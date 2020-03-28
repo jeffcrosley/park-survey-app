61bf3e664b4e4bbed43772773b61ea087ac8a8ec
Jeff Crosley <jeffcrosley@gmail.com>
2020-03-28 09:45:47 -0400

    cleanup; got rid of unused classes and methods

--- module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/model/jdbc/JDBCSurveyDAO.java
+++ module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/model/jdbc/JDBCSurveyDAO.java
@@ -1,17 +1,11 @@
 package com.techelevator.npgeek.model.jdbc;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
-import com.techelevator.npgeek.model.Park;
-import com.techelevator.npgeek.model.ParkSurveys;
 import com.techelevator.npgeek.model.Survey;
 import com.techelevator.npgeek.model.dao.SurveyDAO;
 
@@ -19,7 +13,6 @@ import com.techelevator.npgeek.model.dao.SurveyDAO;
 public class JDBCSurveyDAO implements SurveyDAO {
 
 	private JdbcTemplate jdbcTemplate;
-	private List<Survey> parkSurveys = new ArrayList<Survey>();
 
 	@Autowired
 	public JDBCSurveyDAO(DataSource dataSource) {
@@ -33,47 +26,4 @@ public class JDBCSurveyDAO implements SurveyDAO {
 		jdbcTemplate.update(QUERY_ADD_SURVEY, survey.getParkCode(), survey.getEmailAddress(), survey.getState(),
 				survey.getActivityLevel());
 	}
-
-	
-	
-//	@Override
-//	public List<Park> getAllParks() {
-//		
-//		List<Park> parks = new ArrayList<Park>();
-//		
-//			SqlRowSet results = jdbcTemplate.queryForRowSet(QUERY_GET_ALL_PARKS);
-//			while(results.next()) {
-//				Park getPark = new Park();
-//			}
-//			
-//		return parks;
-//	}
-
-	static final String QUERY_GET_ALL_PARK_SURVEYS =
-			"SELECT *, COUNT (survey_result.parkcode) AS surveys " + 
-			"FROM survey_result " + 
-			"LEFT JOIN park ON park.parkcode = survey_result.parkcode " + 
-			"GROUP BY park.parkcode " + 
-			"ORDER BY surveys DESC";
-	@Override
-	public List<ParkSurveys> getAllParkSurveys() {
-
-		List<ParkSurveys> parkSurveys = new ArrayList<ParkSurveys>();
-		SqlRowSet results = jdbcTemplate.queryForRowSet(QUERY_GET_ALL_PARK_SURVEYS);
-		while(results.next()) {
-			parkSurveys.add(mapRowToSurvey(results));
-		}
-		return parkSurveys;
-	}
-	
-	private ParkSurveys mapRowToSurvey(SqlRowSet results) {
-		ParkSurveys parkSurvey = new ParkSurveys();
-		parkSurvey.setParkCode(results.getString("parkcode"));
-		parkSurvey.setParkName(results.getString("parkname"));
-		parkSurvey.setSurveyCount(results.getInt("count"));
-		return parkSurvey;
-	}
-
-
-
 }
